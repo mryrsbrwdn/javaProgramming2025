02637b943a861f66f341c520efdb5b3eb568b06c
mryrsbrwdn <maryrosebrwdn@gmail.com>
2025-08-19 13:14:26 +0000

    Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+String name;
+String brand;
+double price;
+int quantity;
+
+
+void setPrice(double price){
+    this.price = price;
+
+}
+public static void main (String[] args){
+
+    Main toy =  new Main();
+    toy.name = "Labubu";
+    toy.brand = "Mimisu";
+    toy.quantity = 12;
+    toy.price = 5000;
+    toy.setPrice((toy.price*0.5));
+    System.out.println(toy.setPrice);
+
+}
+
 }
