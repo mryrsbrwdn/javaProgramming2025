123af7ddc2609ebdfa9fb71f5a5d3141bfdd5ce3
mryrsbrwdn <maryrosebrwdn@gmail.com>
2025-08-28 04:37:10 +0000

    task1_aug28

---
+++ task1_primitive.java
@@ -0,0 +1,19 @@
+public class task1_primitive{
+    
+    public static void main(String[] args) {
+        byte zero = 0;
+        int two,number;
+        two=2;
+        number=3110;
+        char point, H;
+        point='.';
+        H='H';
+
+    
+        boolean b = true;
+
+
+        String output ="H"+number+ " w" + zero+ "rld " + "2"+point+zero+" "+b;
+        System.out.println(output);
+    }
+}
